57cff982478669c467fd92cd9644d7a9adecb071
Timothée Benoist <timothee.benoist@etu.univ-cotedazur.fr>
2021-02-10 16:19:22 +0100

    Question 1 TD2

---
+++ TD2/HttpListener/BasicServerHttpListener/Header.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace BasicServerHTTPlistener
+{
+    class Header
+    {
+        private NameValueCollection header;
+        public Header(HttpListenerRequest h)
+        {
+            this.header = h.Headers;
+        }
+        public String displayHeader()
+        {
+            {
+                string html = "<br/>";
+                for (int i = 0; i < this.header.Count; i++)
+                {
+                    html += " Cle : " + this.header.GetKey(i) + " : " + " Valeur : " + this.header.Get(i) + "<br/>";
+                }
+                return html;
+            }
+        }
+    }
+}
