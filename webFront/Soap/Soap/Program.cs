1f6ee26c291ba6e99f43315f282539f639e72eb8
Timothée Benoist <timothee.benoist@etu.univ-cotedazur.fr>
2021-03-10 15:46:23 +0100

    correction conflits + first soap

---
+++ webFront/Soap/Soap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.CalculatorSoap c = new Calculator.CalculatorSoapClient();
+            Console.WriteLine(c.Add(14, 72));
+            Console.ReadLine();
+        }
+    }
+}
